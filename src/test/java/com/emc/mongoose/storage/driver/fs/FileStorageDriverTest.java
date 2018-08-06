18be4dd6c8f91e0a69faccdb8782c654e7e05cf4
veronikaKochugova <veronika.kochugova@gmail.com>
2018-08-06 13:36:48 +0300

    added +x to gradlew

--- src/test/java/com/emc/mongoose/storage/driver/fs/FileStorageDriverTest.java
+++ src/test/java/com/emc/mongoose/storage/driver/fs/FileStorageDriverTest.java
@@ -3,14 +3,10 @@ package com.emc.mongoose.storage.driver.fs;
 import com.emc.mongoose.api.model.item.BasicDataItem;
 import com.emc.mongoose.api.model.item.BasicItemFactory;
 import com.emc.mongoose.api.model.item.DataItem;
-
 import com.github.akurilov.commons.math.Random;
-
 import org.apache.commons.io.FileUtils;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -54,44 +50,32 @@ public class FileStorageDriverTest {
 	@Test
 	public final void testList()
 	throws Exception {
-
 		final String prefix = "yohoho";
 		final int count = 1000;
-
-		for(int i = 0; i < count; i ++) {
+		for(int i = 0; i < count; i++) {
 			if(i < 10) {
-				new File(TMP_DIR_PATH.toString() + File.separatorChar + prefix + "000" + Integer.toString(i))
-					.createNewFile();
+				new File(TMP_DIR_PATH.toString() + File.separatorChar + prefix + "000" +
+					Integer.toString(i)).createNewFile();
 			} else if(i < 100) {
-				new File(TMP_DIR_PATH.toString() + File.separatorChar + prefix + "00" + Integer.toString(i))
-					.createNewFile();
+				new File(
+					TMP_DIR_PATH.toString() + File.separatorChar + prefix + "00" + Integer.toString(i)).createNewFile();
 			} else if(i < 1000) {
-				new File(TMP_DIR_PATH.toString() + File.separatorChar + prefix + "0" + Integer.toString(i))
-					.createNewFile();
+				new File(
+					TMP_DIR_PATH.toString() + File.separatorChar + prefix + "0" + Integer.toString(i)).createNewFile();
 			}
 		}
-
 		final Random rnd = new Random();
-		for(int i = 0; i < count; i ++) {
-			new File(TMP_DIR_PATH.toString() + File.separatorChar + Integer.toString(rnd.nextInt()))
-				.createNewFile();
+		for(int i = 0; i < count; i++) {
+			new File(TMP_DIR_PATH.toString() + File.separatorChar + Integer.toString(rnd.nextInt())).createNewFile();
 		}
-
 		//counting files after "yohoho0099"
-		List<DataItem> items = ListingHelper.list(
-			new BasicItemFactory<>(), TMP_DIR_PATH.toString(), prefix, 10,
+		List<DataItem> items = ListingHelper.list(new BasicItemFactory<>(), TMP_DIR_PATH.toString(), prefix, 10,
 			new BasicDataItem("yohoho0099", 0, 0), count
 		);
 		assertEquals(Integer.toString(items.size()), 900, items.size());
-
-		items = ListingHelper.list(
-			new BasicItemFactory<>(), TMP_DIR_PATH.toString(), prefix, 10, null, 100
-		);
+		items = ListingHelper.list(new BasicItemFactory<>(), TMP_DIR_PATH.toString(), prefix, 10, null, 100);
 		assertEquals(100, items.size());
-
-		items = ListingHelper.list(
-			new BasicItemFactory<>(), TMP_DIR_PATH.toString(), null, 10, null, 2 * count
-		);
+		items = ListingHelper.list(new BasicItemFactory<>(), TMP_DIR_PATH.toString(), null, 10, null, 2 * count);
 		assertEquals(2 * count, items.size());
 	}
 }
--- src/test/java/com/emc/mongoose/storage/driver/fs/WindowsFileSystemTest.java
+++ src/test/java/com/emc/mongoose/storage/driver/fs/WindowsFileSystemTest.java
@@ -5,27 +5,33 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.nio.file.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.junit.Assert.assertTrue;
 
 public class WindowsFileSystemTest {
-    private Path path;
 
-    @Before
-    public void setUp() throws Exception {
-        path = FileSystems.getDefault().getPath("D:", "tmp", "file.txt");
-    }
+	private Path path;
 
-    @Test
-    public final void test() throws Exception {
-        new File(path.toString()).createNewFile();
-        assertTrue(Files.exists(path));
-        System.out.println(path);
-    }
+	@Before
+	public void setUp()
+	throws Exception {
+		path = FileSystems.getDefault().getPath("D:", "tmp", "file.txt");
+	}
 
-    @After
-    public void tearDown() throws Exception {
-        Files.deleteIfExists(path);
-    }
+	@Test
+	public final void test()
+	throws Exception {
+		new File(path.toString()).createNewFile();
+		assertTrue(Files.exists(path));
+		System.out.println(path);
+	}
+
+	@After
+	public void tearDown()
+	throws Exception {
+		Files.deleteIfExists(path);
+	}
 }
